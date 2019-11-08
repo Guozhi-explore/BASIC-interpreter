dc8c98f45cb5ccf85bdebcc69bc934e6d962cb0c
Guozhi-explore <2151259337@qq.com>
2019-11-08 18:34:12 +0800

    finish expression class

--- evalstate.h
+++ evalstate.h
@@ -1,11 +1,21 @@
 #ifndef EVALSTATE_H
 #define EVALSTATE_H
 
+#include<string>
+using namespace std;
+#include<map>
 
 class evalstate
 {
 public:
     evalstate();
+    void setValue(string str,int value);
+    int getValue(string var);
+    bool isDefined(string var);
+
+private:
+    map<string,int> symbolTable;
+
 };
 
 #endif // EVALSTATE_H
--- exp.cpp
+++ exp.cpp
@@ -1,6 +1,109 @@
 #include "exp.h"
-
+#include "assert.h"
 exp::exp()
 {
 
+}
+
+/********************************ConstantExp************************/
+
+ConstantExp::ConstantExp(int val)
+{
+    this->value=val;
+}
+
+int ConstantExp::eval(evalstate &state){
+    return value;
+}
+
+ExpressionType ConstantExp::type()
+{
+    return ExpressionType::CONSTANT;
+}
+
+int ConstantExp::getConstantValue()
+{
+    return value;
+}
+
+
+
+/***************************IdentifierExp*************************/
+
+IdentifierExp::IdentifierExp(string name)
+{
+    this->name=name;
+}
+
+int IdentifierExp::eval(evalstate &state)
+{
+    if(!state.isDefined(name))
+    {
+        printf("%s is undefined",name.c_str());
+        return -1;
+    }
+    return state.getValue(name);
+}
+
+ExpressionType IdentifierExp::type()
+{
+    return ExpressionType::IDENTIFIER;
+}
+
+string IdentifierExp::getIdentifierName()
+{
+    return name;
+}
+
+
+/*********************************CompoundExp*****************************/
+CompoundExp::CompoundExp(string op,exp *lhs,exp *rhs)
+{
+    this->op=op;
+    this->lhs=lhs;
+    this->rhs=rhs;
+}
+
+int CompoundExp::eval(evalstate &state)
+{
+    int rightValue=rhs->eval(state);
+    if(op=="=")
+    {
+        state.setValue(lhs->getIdentifierName(),rightValue);
+        return rightValue;
+    }
+    int leftValue=lhs->eval(state);
+    if(op=="+") return leftValue+rightValue;
+    if(op=="-") return leftValue-rightValue;
+    if(op=="*") return leftValue*rightValue;
+    if(op=="/"){
+        if(rightValue==0)
+        {
+            printf("Division by 0");
+            assert(rightValue!=0);
+        }
+        return leftValue/rightValue;
+    }
+    printf("illegal operator in expression");
+    return 0;
+}
+
+ExpressionType CompoundExp::type()
+{
+    return ExpressionType::COMPOUND;
+}
+
+string CompoundExp::getOperator()
+{
+    return op;
+}
+
+exp *CompoundExp::getLHS()
+{
+    return this->lhs;
+}
+
+exp *CompoundExp::getRHS()
+{
+    return this->rhs;
 }
